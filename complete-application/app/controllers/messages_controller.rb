c9748f978824b348774a47932e4104e981597d56
Alex FusionAuth <alex.patterson@fusionauth.io>
2023-09-11 17:38:11 -0400

    initial

---
+++ complete-application/app/controllers/messages_controller.rb
@@ -0,0 +1,16 @@
+class MessagesController < ApplicationController
+    def index
+      messages = []
+      messages << "Hello"
+  
+      # further claims/authorization checks
+      roles = []
+      if request.env['jwt.payload'] && request.env['jwt.payload']['roles']
+        roles = request.env['jwt.payload']['roles']
+      end
+      if roles.include?('ceo') 
+        messages << "Hiya, boss"
+      end
+      render json: { messages: messages }.to_json, status: :ok
+    end
+  end
