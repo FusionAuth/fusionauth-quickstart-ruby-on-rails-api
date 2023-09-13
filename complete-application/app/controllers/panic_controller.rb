2bab4e243d4b47952a53989e97c5101c73bd3426
Alex FusionAuth <alex.patterson@fusionauth.io>
2023-09-13 12:46:17 -0400

    update controller for post

--- complete-application/app/controllers/panic_controller.rb
+++ complete-application/app/controllers/panic_controller.rb
@@ -1,20 +1,17 @@
 class PanicController < ApplicationController
-    def index
-      # further claims/authorization checks
-      roles = []
-      if request.env['jwt.payload'] && request.env['jwt.payload']['roles']
-        roles = request.env['jwt.payload']['roles']
-      end
-      if (roles & ['teller']).empty?
-        render json: { message: "Proper role not found for user." }.to_json, status: :unauthorized
-        return
-      end
-
-      if request.method == :post
-        render json: { message: "Only POST method is supported." }.to_json, status: :unauthorized
-        return
-      end
-
-      render json: { message: "We've called the police!" }.to_json, status: :ok
-    end
+  def index
+      render json: { message: "Only POST method is supported." }.to_json, status: :unauthorized
   end
+  def create
+    # further claims/authorization checks
+    roles = []
+    if request.env['jwt.payload'] && request.env['jwt.payload']['roles']
+      roles = request.env['jwt.payload']['roles']
+    end
+    if (roles & ['teller']).empty?
+      render json: { message: "Proper role not found for user." }.to_json, status: :unauthorized
+      return
+    end
+    render json: { message: "We've called the police!" }.to_json, status: :ok
+  end
+end
